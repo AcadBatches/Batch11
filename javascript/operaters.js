fbd7fb0f592df2b79a97e6e90a6eed148d044e13
collegemanagement <ushakirankarmi@gmail.com>
2025-01-01 14:13:33 +0530

    operators

---
+++ javascript/operaters.js
@@ -0,0 +1,180 @@
+
+//Arthmetic operator
+//addition
+a=4
+b=5
+console.log(a+b)
+
+//subtraction
+console.log(a-b)
+
+//multiplication               
+console.log(a*b)
+
+//division  
+console.log(a/b)    
+
+//modulus
+console.log(a%b)    
+
+//exponential    
+console.log(a**b)            
+
+//increment                 
+console.log(a++)            
+
+//decrement                         
+console.log(a--)
+
+
+//Assignment operator
+
+// =
+a=10
+b=20
+console.log(a)
+console.log(b)
+
+// +=
+c=a+=b
+console.log(c)
+
+//-=
+c=a-=b
+console.log(c)
+
+// *=
+c=a*=b
+console.log(c)
+
+// /=
+c=a/=b
+console.log(c)
+
+// %=
+c=a%=b
+console.log(c)
+
+// **=
+c=a**=b
+console.log(c)
+
+//shifting operator
+// <<
+a=10
+b=20
+c=a<<b
+console.log(c)
+
+// >>
+c=a>>b
+console.log(c)
+
+// >>>
+c=a>>>b
+console.log(c)
+
+
+// comparison operator
+// ==
+a=10
+b=20
+c=a==b
+console.log(c)
+
+//===
+c=a==b
+console.log(c)
+
+// !=
+c=a!=b
+console.log(c)
+
+// !==  
+c=a!==b
+console.log(c)
+
+// >
+c=a>b
+console.log(c)
+
+// <
+c=a<b
+console.log(c)
+
+// >=
+c=a>=b
+console.log(c)
+
+// <=
+c=a<=b
+console.log(c)
+
+//string operator
+a="hello"
+b="world"
+console.log(a+b)
+
+//logical operator
+
+//&&
+a=10
+b=20
+c=a>b&&a<b
+console.log(c)
+
+//||
+c=a>b||a<b
+console.log(c)
+
+//!=
+c=a!=b
+console.log(c)
+
+//Bitwise operator
+//&
+a=5
+b=6
+c=a&b
+console.log(c)
+
+//|
+c=a|b
+console.log(c)
+
+//^
+c=a^b
+console.log(c)
+
+//~
+c=~a
+console.log(c)
+
+//<<
+c=a<<b
+console.log(c)
+
+//>>
+c=a>>b
+console.log(c)  
+
+//>>>
+c=a>>b
+console.log(c)
+
+//Ternary operator
+a=10
+b=20
+c=a>b?b:a
+console.log(c)
+
+//type oprator
+//typeof
+a=10
+b=20
+c=typeof(a)
+console.log(c)
+
+//instanceof    
+c=a instanceof Number
+console.log(c)
